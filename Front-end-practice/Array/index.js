1f2cd7c27a0ec3605e7010cc4e88bd97af55e9df
amitabh Singh <amitabhsingh128@gmail.com>
2025-12-26 12:20:18 +0530

    feat:js practice with temperature converter.

---
+++ Front-end-practice/Array/index.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "banana", "orange", "watermelon"];
+console.log(fruits);
+// fruits.push("coconut");  append at the end
+// fruits.pop() remove last element
+// fruits.unshift(); add Element to the beginning
+// fruits.shift(); removes Element from the beginning
+//let fruit_length = fruits.length;
+//fruits.indexOf("apple"); first occurence idx, -1 if not
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
